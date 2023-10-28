540ab11ea2bf5f93ff40cc210d962578e393cb9f
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-10-28 17:07:49 +0200

    feat(greeter): Add greeting service

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,12 @@
 import { greeter } from "./greeter";
 import "./styles/main.scss";
 
-const greetingImg = document.getElementById("greetingImg") as HTMLImageElement;
+const greetingImg = document.getElementById("greeting-img") as HTMLImageElement;
 greetingImg.src = require("./assets/greeting.svg") as string;
 
-const user = "TypeScript Developer";
-console.log(greeter(user));
+const greetingBtn = document.getElementById("greeting-btn") as HTMLButtonElement;
+greetingBtn.addEventListener("click", () => {
+    const nameInput = document.getElementById("name") as HTMLInputElement;
+    const greetingDiv = document.getElementById("greeting") as HTMLDivElement;
+    greetingDiv.innerHTML = greeter(nameInput.value);
+});
